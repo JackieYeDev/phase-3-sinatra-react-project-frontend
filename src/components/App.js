795e82c718799cc8dc273f3a95c82120ce6c7d76
Jackie <jye0325@gmail.com>
2022-08-25 23:25:36 -0400

    Add grid formatting to the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { createRef } from "react";
+import NoteViewer from "./NoteViewer";
+import NoteMenu from "./NoteMenu";
+import { Divider, Grid, Segment, Sticky } from "semantic-ui-react";
 
 function App() {
-  return <div></div>;
+  const contextRef = createRef();
+  return (
+    <>
+      <Divider horizontal />
+      <Grid container divided relaxed columns={2} stackable>
+        <Grid.Column width={4}>
+          <Segment>
+            <Sticky context={contextRef}>
+              <NoteMenu />
+            </Sticky>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Segment>
+            <NoteViewer />
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
