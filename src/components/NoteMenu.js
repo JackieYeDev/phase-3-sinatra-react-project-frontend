8b1fa5f0bc052a81be4bcf823b37a658c0e94ada
Jackie <jye0325@gmail.com>
2022-08-26 00:27:39 -0400

    Update NoteMenu and UserSelect wireframe

--- src/components/NoteMenu.js
+++ src/components/NoteMenu.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import UserSelect from "./UserSelect";
+import {
+  Button,
+  ButtonGroup,
+  Image,
+  Input,
+  Loader,
+  Menu,
+  MenuItem,
+} from "semantic-ui-react";
 
-function NoteMenu() {
-  return <div></div>;
+function NoteMenu(props) {
+  const [activeNote, setActiveNote] = useState("");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveNote(name);
+    console.log(e);
+  };
+  return (
+    <Menu vertical fluid>
+      <Menu.Item onClick={handleItemClick}>Notes App</Menu.Item>
+      <Menu.Item>
+        <UserSelect />
+        <Button attached={"bottom"}>Load</Button>
+      </Menu.Item>
+      <Menu.Item>
+        <Input placeholder={"Search by title"}></Input>
+        <Button attached={"bottom"}>Search</Button>
+      </Menu.Item>
+      <Menu.Item>
+        {props.notes ? (
+          ""
+        ) : (
+          <>
+            <Loader active />
+            <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+          </>
+        )}
+      </Menu.Item>
+    </Menu>
+  );
 }
 
 export default NoteMenu;
--- src/components/UserSelect.js
+++ src/components/UserSelect.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Select } from "semantic-ui-react";
 
 function UserSelect() {
-  return <div></div>;
+  const [userList, setUserList] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  useEffect(() => {
+    /*
+     * FETCH userList from Sinatra API after render
+     * {key: "", value: "", text: ""}
+     */
+  }, {});
+  return (
+    <Select
+      placeholder="Filter an existing user"
+      options={[
+        { key: "test", value: "test", text: "test" },
+        { key: "testing", value: "testing", text: "testing" },
+      ]}
+      fluid
+    />
+  );
 }
 
 export default UserSelect;
