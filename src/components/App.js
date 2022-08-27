c6a7713508f07995254e9fd258309749e066c6ae
Jackie <jye0325@gmail.com>
2022-08-26 23:51:53 -0400

    Add useEffect() to fetch from Sinatra API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React, { createRef } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import NoteViewer from "./NoteViewer";
 import NoteMenu from "./NoteMenu";
 import { Divider, Grid, Segment, Sticky } from "semantic-ui-react";
 
 function App() {
   const contextRef = createRef();
+  const [notes, setNotes] = useState([]);
+  const [authors, setAuthors] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9292/notes")
+      .then((res) => res.json())
+      .then((notes) => setNotes(notes))
+      .catch((err) => console.error(err));
+    fetch("http://localhost:9292/authors")
+      .then((res) => res.json())
+      .then((authors) => setAuthors(authors))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <Divider horizontal />
@@ -12,7 +25,7 @@ function App() {
         <Grid.Column width={6}>
           <Segment>
             <Sticky context={contextRef}>
-              <NoteMenu />
+              <NoteMenu authors={authors} notes={notes} />
             </Sticky>
           </Segment>
         </Grid.Column>
